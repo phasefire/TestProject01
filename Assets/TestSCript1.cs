63f2a6825fc8ad9038bc5c1636b3201aa34d4251
MONOLITHV2\RobAdmin <phasefire@gmail.com>
2016-11-23 18:09:08 -0800

    Commit 3 Added another file!

---
+++ Assets/TestSCript1.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestSCript1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("Blatg");
+	}
+}
