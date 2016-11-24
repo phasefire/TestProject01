50360b3eedf9980c1b6fdff6bc63a5a58b501ad2
MONOLITHV2\RobAdmin <phasefire@gmail.com>
2016-11-23 18:07:51 -0800

    Commit Test 2 Added 1 Script

---
+++ Assets/TestSCript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestSCript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("Blatg");
+	}
+}
